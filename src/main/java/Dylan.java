d6306a12780f01f47968d71d680cacf761f593c4
dylanyeo20 <251yeoyq@gmail.com>
2026-01-22 22:18:38 +0800

    Change Chatbot Name, Implement greeting and Exit

---
+++ src/main/java/Dylan.java
@@ -0,0 +1,11 @@
+public class Dylan {
+    public static void main(String[] args) {
+        String chatbot = "Dylan";
+        System.out.println(String.format("____________________________________________________________\n" +
+                " Hello! I'm %s\n" +
+                " What can I do for you?\n" +
+                "____________________________________________________________\n" +
+                " Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________\n", chatbot));
+    }
+}
