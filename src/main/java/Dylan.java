a4ec0bad4ab87f06d4bc2a8e0b0c6d73d29ec2e3
dylanyeo20 <251yeoyq@gmail.com>
2026-01-22 22:38:45 +0800

    Implemented Echo

--- src/main/java/Dylan.java
+++ src/main/java/Dylan.java
@@ -1,11 +1,26 @@
+import java.util.Scanner;
+
 public class Dylan {
     public static void main(String[] args) {
-        String chatbot = "Dylan";
+        String chatbot = "Dylan", line = "____________________________________________________________\n";
         System.out.println(String.format("____________________________________________________________\n" +
                 " Hello! I'm %s\n" +
                 " What can I do for you?\n" +
-                "____________________________________________________________\n" +
-                " Bye. Hope to see you again soon!\n" +
                 "____________________________________________________________\n", chatbot));
+
+        Scanner sc = new Scanner(System.in);
+
+        String input = "";
+        while(!input.equals("bye")) {
+            input = sc.nextLine();
+            System.out.println(line + input + "\n" + line);
+        }
+
+
+        System.out.println(
+                " Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________\n");
+
+
     }
 }
