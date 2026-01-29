d3125bdefd7ce04e12725f0e72e355a316fa8b4e
dylanyeo20 <251yeoyq@gmail.com>
2026-01-30 01:40:09 +0800

    Change Dates from String to LocalDateTime

--- src/main/java/Deadlines.java
+++ src/main/java/Deadlines.java
@@ -1,27 +1,29 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 public class Deadlines extends Task{
-    String by;
-    public Deadlines(String name, String by) {
+    LocalDateTime by;
+    public Deadlines(String name, LocalDateTime by) {
         super(name, 'D');
         this.by = by;
         String output = String.format("Got it. I've added this task:\n %s\nNow you have %d tasks in the list", getStatus(), totalTask());
         System.out.println(output);
     }
 
-    public Deadlines(String name, String by, boolean isTaskDone) {
+    public Deadlines(String name, LocalDateTime by, boolean isTaskDone) {
         super(name, 'D', isTaskDone);
         this.by = by;
     }
 
     @Override
     public String getStatus() {
-        String output = String.format("%s (by: %s)", super.getStatus(), this.by);
+        String output = String.format("%s (by: %s)", super.getStatus(), by.format(DATE_DISPLAY_FORMAT));
         return output;
     }
 
     @Override
     public String dataInputString() {
         int isDone = (super.isDone ? 1 : 0);
-        return type + " | " + isDone + " | " + name + " | " + by;
+        return type + " | " + isDone + " | " + name + " | " + by.format(DATE_DATA_FORMAT);
     }
 
 }
